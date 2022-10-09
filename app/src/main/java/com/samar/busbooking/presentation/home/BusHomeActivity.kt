56f69a7cf961896c00fc15400a484d01654eb503
Samar <iamalsksamar@gmail.com>
2022-10-09 10:41:18 +0530

    Initial commit

---
+++ app/src/main/java/com/samar/busbooking/presentation/home/BusHomeActivity.kt
@@ -0,0 +1,28 @@
+package com.samar.busbooking.presentation.home
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.samar.busbooking.presentation.ui.theme.BusBookingTheme
+
+class BusHomeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BusBookingTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
